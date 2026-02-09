4e7b692f78a593df4d7dca2b8c5f39d41ff7c732
TatianaChF <tatiana.chefox@gmail.com>
2026-02-09 13:30:11 +0300

    install pinia and vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {createPinia} from "pinia";
+import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
+    components,
+    directives,
+})
+
+app.use(pinia).use(vuetify).mount('#app')
