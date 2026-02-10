8e5765de1cca8a9519affedaab1dc7128a6d3637
TatianaChF <tatiana.chefox@gmail.com>
2026-02-10 15:40:51 +0300

    move the initialization of Pinia and Vuetify to a separate folder

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createPinia } from "pinia";
-import { createVuetify } from 'vuetify';
-import { aliases, mdi } from 'vuetify/iconsets/mdi';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 import '@mdi/font/css/materialdesignicons.css';
+import pinia from "./utils/pinia.ts";
+import vuetify from "./utils/vuetify.ts";
 
-const pinia = createPinia();
 const app = createApp(App);
-const vuetify = createVuetify({
-    icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-            mdi,
-        }
-    },
-    components,
-    directives,
-})
 
 app.use(pinia).use(vuetify).mount('#app')
---
+++ src/utils/vuetify.ts
@@ -0,0 +1,18 @@
+import {createVuetify} from "vuetify/framework";
+import {aliases, mdi} from "vuetify/iconsets/mdi";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
+    components,
+    directives,
+});
+
+export default vuetify;
