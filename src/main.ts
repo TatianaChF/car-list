5f4ba57fc5d40693e309946891c54d01000094e0
TatianaChF <tatiana.chefox@gmail.com>
2026-02-10 14:22:56 +0300

    add import

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import {createPinia} from "pinia";
+import { createPinia } from "pinia";
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
 const pinia = createPinia();
 const app = createApp(App);
